759d21cdc35f63dfac4f4c1f3b4ed2bf42ba8075
tuannguyen <coixaygio107@gmail.com>
2024-12-05 00:33:37 +0700

    change routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,16 @@
-
-import config from "~/config";
+import config from '~/config';
 
 //pages
-import EBook from "~/pages/EBook";
-import Home from "~/pages/Home";
-import SearchEbooks from "~/pages/SearchEbooks";
+import EBook from '~/pages/EBook/EBook';
+import Home from '~/pages/Home/Home';
+import SearchEbooks from '~/pages/SearchEbooks/SearchEbooks';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.ebook, component: EBook },
     { path: config.routes.search, component: SearchEbooks },
-]
+];
 
-const privateRoutes = []
+const privateRoutes = [];
 
-export { publicRoutes, privateRoutes}
+export { publicRoutes, privateRoutes };
