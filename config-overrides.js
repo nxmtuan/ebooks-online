612f235a21f8123db10424adede87debd06eac49
tuannguyen <coixaygio107@gmail.com>
2024-12-05 00:18:41 +0700

    change alias config

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,17 @@
-const { override, useBabelRc } = require("customize-cra");
+// const { override, useBabelRc } = require("customize-cra");
+
+// module.exports = override(
+//     // eslint-disable-next-line react-hooks/rules-of-hooks
+//     useBabelRc()
+// );
+
+const path = require('path');
+const { override, useBabelRc, addWebpackAlias } = require('customize-cra');
 
 module.exports = override(
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useBabelRc()
+    useBabelRc(),
+    addWebpackAlias({
+        '~': path.resolve(__dirname, 'src'),
+    }),
 );
